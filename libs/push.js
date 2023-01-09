7b8fd9b356ffe569109a6a58ce54be2e2d757d95
XINGMOUREN <2638526782@qq.com>
2023-01-09 13:19:18 +0800

    修改api接口

--- libs/push.js
+++ libs/push.js
@@ -3,14 +3,15 @@ var qcloudSDK = require('./submit');
 var config = this.config;
 var secret_Id = config.tencentcdn.secretId;
 var secret_Key = config.tencentcdn.secretKey;
-var url = config.refreshurl
+var url = config.url
 qcloudSDK.config({
     secretId: secret_Id,
     secretKey: secret_Key
 })
 
-qcloudSDK.request('RefreshCdnUrl', {
-    'urls.0': url
+qcloudSDK.request('RefreshCdnDir', {
+    'dirs.0': url,
+    "type": "1"
 }, (res) => {
 console.log('cdn刷新结果' + res);
 console.log('url:' + url);
